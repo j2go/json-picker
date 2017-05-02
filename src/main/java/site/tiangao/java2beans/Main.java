b75afa6537d5e0c2b6bdff79f5469daf75c43b33
shitg <tiangao1102@163.com>
2017-05-02 16:34:35 +0800

    init

---
+++ src/main/java/site/tiangao/java2beans/Main.java
@@ -0,0 +1,31 @@
+package site.tiangao.java2beans;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+/**
+ * Created by shitiangao on 2017/5/2.
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+            server.createContext("/", (exchange) -> {
+                String response = "Hello World!";
+                exchange.sendResponseHeaders(200, response.length());
+
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            });
+            server.setExecutor(null); // creates a default executor
+            server.start();
+            System.out.println("start");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/resources/bean.ftl
@@ -0,0 +1,21 @@
+package ${package};
+
+import lombok.Data;
+
+/**
+* Created by ${auther} on ${date}.
+*/
+@Data
+public class ${className} {
+
+    private int intKey;
+
+    private long longKey;
+
+    private double doubleKey;
+
+    private boolean boolKey;
+
+    private String strKey;
+
+}
